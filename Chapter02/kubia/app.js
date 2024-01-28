8880b0694ca92b1b681a7e33e76a4c2547d4c1dc
173823220-myseneca <157029534+173823220-myseneca@users.noreply.github.com>
2024-01-28 13:47:54 -0500

    Updated display message

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("You've hit " + "v0.2 of this application" + "\n");
 };
 
 var www = http.createServer(handler);
